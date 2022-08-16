db704a4c7fefd7c502e6eec6b1c1c31d05cf0e8e
EliasVal <46247153+EliasVal@users.noreply.github.com>
2022-08-17 01:21:54 +0300

    Further fixes

--- ts/clientCommands/catt.ts
+++ ts/clientCommands/catt.ts
@@ -1,9 +1,9 @@
-import { Message } from 'discord.js';
+import { Message, TextChannel } from 'discord.js';
 
 module.exports = {
   name: 'catt',
   description: 'removes the catt role and give it back after a defined delay',
-  run: function (client: _Client, message: Message, args: string[]) {
+  run: async function (client: _Client, message: Message, args: string[]) {
     if (message.member?.roles.cache.has('931329640914616320')) {
       //let userId = args[0];
       let catDuration = parseInt(args[1]);
@@ -11,12 +11,14 @@ module.exports = {
       const logChan = '737631798921527366';
       const catRole = '951376685247365121';
 
-      message.mentions.members?.forEach((member) => {
+      message.mentions.members?.forEach(async (member) => {
         let logMsg = `moderator: ${message.author} removed the catt role from ${member} for ${catDuration}`;
         //const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-        member.roles.remove(catRole, logMsg);
-        //client.channels.fetch(logChan).then(channel => channel.send(logMsg));
-        message.reply('done!!');
+        await member.roles.remove(catRole, logMsg);
+        // @ts-ignore
+        let channel: TextChannel = await client.guilds.cache.get(message.guild?.id).channels.fetch(logChan); //.then(channel => channel.send(logMsg));
+        channel?.send(logMsg);
+        await message.reply('done!!');
 
         //timeout in miliseconds
         setTimeout(() => {
