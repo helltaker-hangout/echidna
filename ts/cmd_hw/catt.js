bdef294d8086755ca64740cc86c0656e62e2cfdf
lemonlyme <108959314+lemonlyme@users.noreply.github.com>
2022-08-17 00:06:18 +0200

    Create catt.js

---
+++ ts/cmd_hw/catt.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: 'catt',
+    description: "removes the catt role and give it back after a defined delay",
+    execute(message, args){
+	if(message.member.roles.cache.has('931329640914616320')){
+		
+		//let userId = args[0];
+		let catDuration = args[1];
+		const logChan = '737631798921527366';
+		const catRole = '951376685247365121';
+		
+		
+		message.mentions.members.forEach(member => {
+			let logMsg = `moderator: ${message.author} removed the catt role from ${member} for ${catDuration}`;
+			//const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+			member.roles.remove(catRole, logMsg);
+			//client.channels.fetch(logChan).then(channel => channel.send(logMsg));
+			message.reply('done!!');
+			
+			//timeout in miliseconds
+			setTimeout(() => { 
+				member.roles.add(catRole, 'giving the role back');
+				
+			}, catDuration);
+			
+		})
+		
+		
+	} else {
+			message.channel.send('you do not have the permission to execute that command');
+			
+		}
+    }
+}
