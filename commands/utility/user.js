db7f6a723d43605765182b9c49ec3b8d69195c4d
Gardy <vovka20011@gmail.com>
2024-01-26 20:30:14 +0300

    /user now suggests a user to check time of creation and join.

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,9 +1,13 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('user')
-        .setDescription('Provides information about the user.'),
+        .setDescription('Provides information about the user.')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User name')
+                .setRequired(true)),
     async execute(interaction) {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
